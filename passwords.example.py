2b7934ebff8acaf308d721da06932d45e3b4f27a
fred ver <fred.ver06@gmail.com>
2025-07-22 20:59:21 +0200

    Improved readability of passwords.example.py

--- passwords.example.py
+++ passwords.example.py
@@ -1,13 +1,15 @@
-app_secret_key = "your_flask_secret_key"
+# Create a copy of this file in the same directory, and call it "passwords.py", then personalize the configuration
+
+app_secret_key = "your_flask_secret_key" # generate a long alphanumeric string and put it here. Never divulge it, because whoever has it can forge false user sessions, that means that they can log in as any user and expose their data.
 
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'frediet_user',
-    'password': 'your frediet_user password',
+    'password': 'your frediet_user password', # change this to the actual password you decided to use for the frediet_user
     'database': 'frediet',
     'charset': 'utf8mb4'
 }
 
-flask_port = 15346
+flask_port = 15346 # Don't change this. If the app's errors complain that the port is occupied, then change this, and remember to also update the port in your reverse proxy's configuration
 
-flask_environment = 'development' # 'production'
+flask_environment = 'production' # don't change this. If you're a developer who's looking to modify the code, set this to this exact string: 'development'
